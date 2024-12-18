493afd8a04e9009b0570cb729735c0ff061144ef
Jean Tuffier <jean.tuffier@pm.me>
2024-12-19 00:04:06 +0100

    Move the presenter call to CounterScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,9 +50,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    implementation(platform(libs.koin.bom))
-    implementation(libs.koin.core)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
